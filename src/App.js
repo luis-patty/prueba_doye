cbe4960cdf4d3af8ef1c57a48c0d7e9401b176a3
Luis Patty <luispatty@Tecnicos-MacBook-Pro.local>
2023-02-07 18:19:16 -0300

    feat(app.js):styles-chakra

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ChakraProvider, VStack } from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Text,
+  List,
+  ListItem,
+  ListIcon,
+} from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const callApi = (async) => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => response.json())
+      .then((allpokemon) => {
+        console.log(allpokemon);
+        setData(allpokemon.results);
+        //console.log(allpokemon.results[0].name);
+        console.log("allpokemon.results", allpokemon.results);
+      });
+  };
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack margin="50px 50px 50px 50px">
+      <ChakraProvider>
+        <FormControl>
+          <FormLabel>Email address</FormLabel>
+          <Input type="email" />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+        </FormControl>
+      </ChakraProvider>
+
+      <List spacing={3}>
+        {data.map(({ name, url }) => (
+          <ListItem key={url}>
+            <ListIcon as={CheckIcon} color="green.500" /> {name}
+          </ListItem>
+        ))}
+      </List>
+    </VStack>
   );
 }
 
